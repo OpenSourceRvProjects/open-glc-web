c6038ef692f022d96fbd970fe91c0c5cdbb4bbd1
dev-rvdl <dev-rvdl@outlook.es>
2025-02-09 16:14:44 -0600

    Security tokens validations

--- OpenGLC.MVC/Program.cs
+++ OpenGLC.MVC/Program.cs
@@ -1,8 +1,34 @@
+using OpenGLC.MVC;
+using OpenGLC.MVC.Security;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddMvc()
+		.AddSessionStateTempDataProvider();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession();
+
+builder.Services.AddHttpContextAccessor();
+
+
+////Google search: services AddScopped instanciate
+////Initialize the Instances within ConfigServices in Startup .NET
+////https://www.thecodebuzz.com/initialize-instances-within-configservices-in-startup/
+////https://www.roundthecode.com/dotnet/how-to-read-the-appsettings-json-configuration-file-in-asp-net-core
+var securityKeys = builder.Configuration.GetSection("security").Get<ISecurityKeys>();
+
+builder.Services.AddSingleton<ISecurityKeys>((serviceProvider) =>
+{
+	return securityKeys;
+});
+
+builder.Services.AddCustomAuthentication(securityKeys.JWT_PrivateKey);
+
 
 var app = builder.Build();
 
@@ -12,6 +38,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
---
+++ OpenGLC.MVC/StartupExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace OpenGLC.MVC
+{
+	public static class StartupExtensions
+	{
+		public static void AddCustomAuthentication(this IServiceCollection services, string JWTKey)
+		{
+			var mySecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JWTKey));
+			services.AddAuthentication(x =>
+			{
+				x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(x =>
+			{
+				x.RequireHttpsMetadata = false;
+				x.SaveToken = true;
+				x.TokenValidationParameters = new TokenValidationParameters()
+				{
+					ValidateIssuerSigningKey = true,
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidateLifetime = true,
+					RequireExpirationTime = true,
+					ValidIssuer = "openglclevel-app",
+					ValidAudience = "openglclevel-app",
+					IssuerSigningKey = mySecurityKey,
+					//https://stackoverflow.com/questions/43045035/jwt-token-authentication-expired-tokens-still-working-net-core-web-api
+					ClockSkew = TimeSpan.Zero
+				};
+			});
+		}
+	}
+}
