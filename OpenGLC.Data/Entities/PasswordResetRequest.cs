ea63ba3ff713272abe13f2af523df07832ce4b0e
dev-rvdl <dev-rvdl@outlook.es>
2025-02-26 23:11:15 -0600

    Scaffold DB context for LoginRequests

---
+++ OpenGLC.Data/Entities/PasswordResetRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenGLC.Data.Entities;
+
+public partial class PasswordResetRequest
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+    public DateTime ExpirationDate { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public Guid UserId { get; set; }
+
+    public bool Status { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
